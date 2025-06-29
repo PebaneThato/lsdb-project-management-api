32da6bcda762e46c4ef2e55dc450aeb6c0f8ee75
Thato Pebane <jpebane@gmail.com>
2025-06-29 20:04:04 +0200

    Add Project Database SQL Structure

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS user_registration (
+CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
@@ -8,3 +8,22 @@ CREATE TABLE IF NOT EXISTS user_registration (
     user_role VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS project (
+    project_id INT AUTO_INCREMENT PRIMARY KEY,
+    project_name VARCHAR(255) NOT NULL,
+    project_start_date DATE,
+    project_end_date DATE,
+    project_description TEXT,
+    project_created_by INT,
+    project_assigned_to INT,
+    
+    FOREIGN KEY (project_created_by) REFERENCES user(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+        
+    FOREIGN KEY (project_assigned_to) REFERENCES user(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
